cbedda67ff5913b343b7138fb24544355df8e0c1
Amanda Treutler <amandathedev@gmail.com>
2019-07-09 15:27:25 -0500

    Modified message

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,21 +1,22 @@
 #!/usr/bin/env ruby
-
+require "pry"
 # Code your solution here!
 def run_guessing_game
-    loop do
-        puts "Guess a number between 1 and 6."
-        user_guess = gets.chomp
-        rand_int = rand(1..6).to_s
-        if user_guess == "exit"
-            puts "Goodbye!"
-            break
-        elsif user_guess == rand_int
-            puts "You guessed the correct number!"
-            # break
-        else user_guess != rand_int
-            puts "The computer guessed #{rand_int}."
-            # break
-        end
-        break
+  user_guess = ""
+  while user_guess != "exit"
+    puts "Guess a number between 1 and 6."
+    user_guess = gets.chomp
+    rand_int = rand(1..6).to_s
+    binding.pry
+    if user_guess == rand_int
+      puts "You guessed the correct number!"
+      # break
+    else
+      puts "The computer guessed #{rand_int}."
+      # break
     end
+  end
+  puts "Goodbye! Thanks for playing."
 end
+
+run_guessing_game
